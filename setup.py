1bf7dd8e83d315de8dc2e056a53e4a01767a730c
Andrés Villavicencio <andres@unholster.com>
2013-10-01 12:28:41 -0300

    Preparing milieu for deployment to pypi

--- setup.py
+++ setup.py
@@ -1,14 +1,27 @@
+# coding=utf_8
+import os
 from setuptools import setup, find_packages
 
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
 setup(
-    name="milieu",
-    version="1.0.0",
+    name= "unholster.milieu",
+    version= "1.0.0",
+    author = u"Sebastian Acuña",
+    author_email = "sebastian@unholster.com",
+    maintainer = u"Andrés Villavicencio",
+    maintainer_email = "andres@unholster.com",
     packages = find_packages(),
+    license = "BSD",
     package_data={
         'milieu' : [
             '*.*',
         ]
     },
+    url="https://github.com/Unholster/milieu",
     install_requires =[],
-    long_description="Flexible parameter management"
+    description="Flexible parameter management",
+    long_description=read("README.md"),
+    classifiers=["Development Status :: 5 - Production/Stable", "Topic :: Utilities"]
 )
